e3046c9b4b5a5802bf536475f392dd86b066f666
Tym154 <159148113+Tym154@users.noreply.github.com>
2025-05-01 14:35:16 +0200

    updated particle class

--- main/particle_class.py
+++ main/particle_class.py
@@ -1,7 +1,7 @@
 class Particle:
-    def __init__(self, color, weight, velocity_x, velocity_y, p_type):
+    def __init__(self, color, density, velocity_x, velocity_y, p_type):
         self.color = color
-        self.weight = weight
+        self.density = density
         self.velocity_x = velocity_x
         self.velocity_y = velocity_y
         self.p_type = p_type
